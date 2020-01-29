5f93e574e5139aa383d9c42ba3609217fa11bf20
jerome valette <valettejerome31@gmail.com>
2020-01-29 12:54:50 +0100

    first commit

---
+++ src/main/java/com/gourmandine/GourmandineApplication.java
@@ -0,0 +1,13 @@
+package com.gourmandine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmandineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmandineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gourmandine/GourmandineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourmandine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmandineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
